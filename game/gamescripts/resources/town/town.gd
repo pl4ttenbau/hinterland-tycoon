1ca8971dd5dea447e8f040634d61ac31aa404f9c
Cornelius Linder <cornelius.linder@onesty-direct.de>
2025-06-18 17:35:14 +0200

    added chapel; non-minor towns start with 5 houses

--- game/gamescripts/resources/town/town.gd
+++ game/gamescripts/resources/town/town.gd
@@ -1,28 +1,35 @@
 @tool
+@icon("res://assets/icons/icon_town_white.png")
 class_name TownResource extends GameObject
 
 @export var town_name: String;
 @export var pos_xz: Vector2
 @export var totalPops: int
+@export var is_minor: bool = false
 
 @export_storage var structures: Array[BaseStructure] = []
 @export_storage var stations: Array[RailStationResource] = []
 
+func _init():
+	super(Enums.EntityTypes.TOWN)
+
 func _to_string():
 	return "<Town %s>" % self.town_name
+	
+func get_initial_bld_count() -> int:
+	if self.is_minor: return 3
+	else: return 5
 	
 func add_building(_building: BaseStructure):
 	self.structures.append(_building)
 	_building.town = self
-	
-func _init():
-	super(Enums.EntityTypes.TOWN)
 
 # == CONSTRUCTOR METHODS ==
-static func of(_name: String, _pos2: Vector2, pops = null) -> TownResource:
+static func of(_name: String, _pos2: Vector2, pops = null, _minor: bool = false) -> TownResource:
 	var instance: TownResource = TownResource.new()
 	instance.town_name = _name
 	instance.pos_xz = _pos2
+	instance.is_minor = _minor
 	if pops && typeof(pops) == TYPE_ARRAY && pops.size() == 2:
 		instance.peasantPops = pops.get(0)
 		instance.bourgiePops = pops.get(1)
@@ -34,5 +41,8 @@ static func from_json(_jsonDict: Dictionary) -> TownResource:
 		push_warning("Town %s has no known position" % _jsonDict["name"])
 		return null
 	else:	
+		var town_name: String = _jsonDict["name"]
 		var posXZ: Vector2 = Vector2(float(townPosArr[0]), float(townPosArr[1]))
-		return TownResource.of(_jsonDict["name"], posXZ)
+		var pops = null
+		var is_minor: bool = _jsonDict.get("isMinor", false)
+		return TownResource.of(town_name, posXZ, pops, is_minor)
