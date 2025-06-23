2192d5b89202d5eb954349b569ad3a9a96b27ecc
Cornelius Linder <cornelius.linder@onesty-direct.de>
2025-06-23 20:37:49 +0200

    map meshes update & new industry meshes

---
+++ game/gamescripts/services/spawners/map_loader.gd
@@ -0,0 +1,1 @@
+class_name MapLoader extends Node
--- game/gamescripts/services/typeloaders/industry_type_loader.gd.gd
+++ game/gamescripts/services/typeloaders/industry_type_loader.gd.gd
@@ -6,19 +6,19 @@ func _init() -> void:
 func make_types() -> Array[IndustryType]:
 	var industry_types: Array[IndustryType] = [
 		# producers
-		IndustryType.new("FARM", "Farm", 8, [], ["4xgrains"]),
-		IndustryType.new("COAL_MINE", "Coal Mine", 20, [], ["4xcoal"]),
-		IndustryType.new("ICON_MINE", "Coal Mine", 30, [], ["6xiron"]),
-		IndustryType.new("QUARRY", "Quarry", 12, [], ["8xstone"]),
-		IndustryType.new("FISHERY", "Fishery", 9, [], ["5xfish"]),
-		IndustryType.new("FOREST", "Forest", 9, [], ["10xwood"]),
+		IndustryType.new("FARM", "Farm", "windmill", 8, [], ["4xgrains"]),
+		IndustryType.new("COAL_MINE", "Coal Mine", "mine", 20, [], ["4xcoal"]),
+		IndustryType.new("ICON_MINE","Coal Mine", "mine", 30, [], ["6xiron"]),
+		IndustryType.new("QUARRY", "Quarry", "mine", 12, [], ["8xstone"]),
+		IndustryType.new("FISHERY", "Fishery", "null",  9, [], ["5xfish"]),
+		IndustryType.new("FOREST", "Forest", "null", 9, [], ["10xwood"]),
 		# transformers
-		IndustryType.new("SMELTER", "Smelter", 22, ["3xiron", "3xcoal"], ["3xsteel"]),
-		IndustryType.new("SAWMILL", "Sawmill", 12, ["9xwood"], ["3xboards"]),
-		IndustryType.new("BAKERY", "Bakery", 5, ["5xgrains"], ["5xfood"]),
-		IndustryType.new("BUTCHER", "Butcher", 9, ["3xfish"], ["3xmeat"]),
+		IndustryType.new("SMELTER", "Smelter", "generic_small", 22, ["3xiron", "3xcoal"], ["3xsteel"]),
+		IndustryType.new("SAWMILL", "Sawmill", "warehouse_1", 12, ["9xwood"], ["3xboards"]),
+		IndustryType.new("BAKERY", "Bakery", "generic_small", 5, ["5xgrains"], ["5xfood"]),
+		IndustryType.new("BUTCHER", "Butcher", "warehouse_1", 9, ["3xfish"], ["3xmeat"]),
 		# consumers
-		IndustryType.new("COAL_PLANT", "Coal Plant", 20, ["4xcoal"], []),
+		IndustryType.new("COAL_PLANT", "Coal Plant", "cooling_tower", 20, ["4xcoal"], []),
 	]
 	GameTypes.industry_types = industry_types
 	return industry_types
