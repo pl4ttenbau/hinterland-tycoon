92b9292a757e27ca4389141b050ade7a05a4a7e1
Cornelius Linder <cornelius.linder@onesty-direct.de>
2025-07-07 16:51:56 +0200

    Refactoring in RailVehicle & Speedometer

--- game/gamescripts/ui/speedometer/speedometer.gd
+++ game/gamescripts/ui/speedometer/speedometer.gd
@@ -4,6 +4,7 @@ const LINE_ROTATION_ZERO := -35.0
 const LINE_ROTATION_MAX := 35.0
 
 @export_storage var current_vehicle: RailVehicle
+
 @export_storage var current_speed: float = 0.0:
 	set(value):
 		current_speed = value
@@ -21,19 +22,26 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if self.visible && self.current_vehicle:
 		self.current_speed = self.current_vehicle.motor.current_speed_percentage
-
+	
+func adjust_speedometer():
+	var angle: float = self.current_speed * self.get_angle_range() + LINE_ROTATION_ZERO
+	self.get_speed_line().rotation_degrees = angle
+	
+#region Vehicle Callbacks
 func _vehicle_entered(vehicle: RailVehicle):
 	self.current_vehicle = vehicle
 	self.visible = true
 	
 func _vehicle_exited():
+	self.current_vehicle = null
+	self.current_speed = 0.0
 	self.visible = false
-	
-func adjust_speedometer():
-	var angle: float = self.current_speed * 70 - 35
-	self.get_speed_line().rotation_degrees = angle
+#endregion
 	
 #region Getters
 func get_speed_line() -> Line2D:
 	return $SpeedLine
+	
+func get_angle_range() -> float:
+	return absf(LINE_ROTATION_ZERO) + absf(LINE_ROTATION_MAX)
 #endregion
