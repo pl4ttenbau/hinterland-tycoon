6583fbee3e8b3ddee66526c0a896101bc3c2a891
Cornelius Linder <cornelius.linder@onesty-direct.de>
2025-07-16 21:54:17 +0200

    further changes in railway track path-mesh scene

--- game/gamescripts/nodes/infr/outer_rail_track.gd
+++ game/gamescripts/nodes/infr/outer_rail_track.gd
@@ -2,27 +2,21 @@
 @icon("res://assets/icons/icon_rail_track.png")
 class_name OuterRailTrack extends VisibleObject
 
+const SCENE_PATH = "res://assets/meshes/infr/rail/rail_track_normal/path_rail_normal_bedded.tscn"
+
 @export var track: RailTrackData:
 	set(value):
 		self.entity = value
-		self.get_path_3d().curve = value.curve
+		$TrackPath.curve = value.curve
 		self.assign_node_names()
 
-const SCENE_PATH = "res://scenes/subscenes/infr/rail_path_mesh_3d.tscn"
-
 func assign_node_names():
-	var track_num: int = self.entity.num
-	self.name = "RailTrack_%d_Container" % track_num
-	self.get_path_3d().name = "RailTrack_%d_Path" % track_num
-	self.get_rail_mesh().name = "RailTrack_%d_Mesh" % track_num
+	self.name = "RailTrack_%d_Container" % self.entity.num
+	$TrackPath.name = "RailTrack_%d_Path" % self.entity.num
+	$BedPathMesh.name = "RailTrack_%d_BeddingMesh" % self.entity.num
+	$TrackPathMesh.name = "RailTrack_%d_TracksMesh" % self.entity.num
 
-func get_path_3d() -> Path3D:
-	return self.get_child(0) as Path3D
-	
-func get_rail_mesh() -> PathMesh3D:
-	return self.get_child(1)
-
-## used for fining center point for distance calculations
+## used for finding center point for distance calculations
 func get_middle_pos() -> Vector3:
 	var road_vertice_count: int = self.entity.vertices.size()
 	var middle_index: int = floori(road_vertice_count /2)
