68d79d1c4eb0cbab04dccf183815022ce91d9c3d
Cornelius Linder <cornelius.linder@onesty-direct.de>
2025-07-23 16:26:55 +0200

    some refactoring in TerrainContainer

--- game/gamescripts/services/spawners/terrain_container.gd
+++ game/gamescripts/services/spawners/terrain_container.gd
@@ -1,13 +1,16 @@
 @icon("res://assets/icons/icon_terrain_white.png")
 class_name TerrainContainer extends Node
 
+@export var map_key: String
 @export var terrain: Terrain3D
 
 signal world_update()
 
 func _enter_tree() -> void:
-	self.world_update.connect(Callable(self, "_on_world_update"))
+	# self-register in GlobalState
 	GlobalState.terrain = self
+	# connect to signals
+	self.world_update.connect(Callable(self, "_on_world_update"))
 
 func _ready() -> void:
 	var terrain_3d_obj: Terrain3D = self.get_child(0)
@@ -21,13 +24,11 @@ func get_height_at(abs_pos: Vector3) -> float:
 	return self.terrain.data.get_height(abs_pos)
 
 func get_pos_at_height(abs_pos: Vector3) -> Vector3:
-	var y: float = get_height_at(abs_pos)
-	return Vector3(abs_pos.x, y, abs_pos.z)
+	return Vector3(abs_pos.x, get_height_at(abs_pos), abs_pos.z)
 
 func _on_world_update() -> void:
 	pass
 	
 func raycast_xz(world_xz: Vector2) -> TerrainRaycastResult:
-	var raycaster: TerrainRaycaster = $TerrainRaycaster
-	var result: TerrainRaycastResult = raycaster.shoot_ray(world_xz)
+	var result: TerrainRaycastResult = $TerrainRaycaster.shoot_ray(world_xz)
 	return result
