aa1029e3666e293214d82da9ae848ceb89bff1cd
Cornelius Linder <cornelius.linder@onesty-direct.de>
2025-09-12 16:20:35 +0200

    MPSB: map streets extension

--- game/gamescripts/services/editor/editor_object_container.gd
+++ game/gamescripts/services/editor/editor_object_container.gd
@@ -1,6 +1,13 @@
+@tool
 @icon("res://assets/icons/icon_editor.png")
 class_name EditorObjectContainer extends Node3D
+
+@export_tool_button("Regenerate Infr.")
+var position_action = do_empty_infr
 
 func _ready():
 	if ! Engine.is_editor_hint():
 		self.visible = false
+
+func do_empty_infr() -> void:
+	pass
