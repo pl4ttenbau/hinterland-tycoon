715039953bead3644b7e96ca2cf626a694c68c53
Cornelius Linder <cornelius.linder@onesty-direct.de>
2025-09-21 18:28:20 +0200

    new villages & houses; MPSB track from Wegezin to Medow; Friedland with houses & streets in Anklam

--- game/gamescripts/services/typeloaders/residential_bld_type_loader.gd
+++ game/gamescripts/services/typeloaders/residential_bld_type_loader.gd
@@ -16,6 +16,7 @@ static func _dict_to_obj(_bld_type_data: Dictionary) -> Array[ResBldType]:
 		ResBldType.new("village_house_3", "Village House", 4),
 		ResBldType.new("half_timbered_1", "Tudor House", 6),
 		ResBldType.new("half_timbered_barn", "Tudor Barn", 5),
+		ResBldType.new("northern_german_house", "Longhouse", 12),
 		# ResBldType.new("city_manor", "City Manor", 12)
 	]
 	
