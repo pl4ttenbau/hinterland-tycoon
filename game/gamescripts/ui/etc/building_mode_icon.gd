6fa96e47901a5ac20645e666c344ad8f8cb1a481
Cornelius Linder <cornelius.linder@onesty-direct.de>
2026-02-16 20:56:12 +0100

    exit building mode with [ESC] instead of timeout

--- game/gamescripts/ui/etc/building_mode_icon.gd
+++ game/gamescripts/ui/etc/building_mode_icon.gd
@@ -7,9 +7,9 @@ func _on_building_mode_switched(enabled: bool):
 	self.visible = enabled
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventKey && UiState.building_mode_on:
-		if event.pressed && event.keycode == KEY_ESCAPE:
-			self.disable_building_mode()
+	if event.is_action_pressed("ui_cancel"):
+		self.disable_building_mode()
+		get_viewport().set_input_as_handled()
 			
 func disable_building_mode():
 	UiState.building_mode_on = false
