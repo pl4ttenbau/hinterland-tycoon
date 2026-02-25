b7689bffb19b58be78a257e2ff735822cc72b8c5
Cornelius Linder <cornelius.linder@onesty-direct.de>
2026-02-25 17:01:24 +0100

    MPSB: road above Stolpe; new design for station & industry signs

--- game/gamescripts/nodes/town/town_sign_panel.gd
+++ game/gamescripts/nodes/town/town_sign_panel.gd
@@ -1,4 +1,11 @@
+@tool
 class_name TownSignPanel extends PanelContainer
+
+@export var town3d: Town3D:
+	get(): return town3d
+	set(value):
+		town3d = value
+		
 
 @export var town_name: String:
 	get(): return town_name
@@ -11,3 +18,13 @@ class_name TownSignPanel extends PanelContainer
 	set(value):
 		pops_amount = value
 		%PopsAmountLabel.text = str(value)
+
+func _set_town(_town3d: Town3D):
+	self.town3d = _town3d
+	if ! _town3d.town:
+		Loggie.error("Cannot find TownData in Town3D \"%s\"" % self.get_path())
+		return
+	if town3d.town.town_name:
+		self.town_name = town3d.town.town_name
+	if town3d.town.totalPops:
+		self.pops_amount = town3d.town.totalPops
