5e477ad0d5b884ad84fd45099322acce97e5c368
Cornelius Linder <cornelius.linder@onesty-direct.de>
2026-03-15 07:06:10 +0100

    VehicleControl: speed change also shows up on speed-setter (although incorrectly)

--- game/gamescripts/ui/vehicle_control/speed_control.gd
+++ game/gamescripts/ui/vehicle_control/speed_control.gd
@@ -7,6 +7,10 @@ class_name SpeedControl extends Control
 		return curr_train
 	set(value):
 		curr_train = value
+		
+@export_storage var curr_speed: VehicleSpeed:
+	get():
+		return self.curr_train.motor.speed
 
 func _enter_tree() -> void:
 	self.gui_input.connect(Callable(self, "_on_gui_input"))
@@ -17,9 +21,12 @@ func _enter_tree() -> void:
 func set_vehicle_speed(speed_percent: float):
 	var speed_float := self._clamp_speed(1 - (speed_percent / 100))
 	Loggie.info("Setting vehicle speed perc: %f" % speed_float)
-	self.curr_train.motor.speed.target = speed_float
+	self.curr_speed.target = speed_float
+	# self._set_handle_position(speed_float)
+	
+func _set_handle_position(speed_float: float):
 	# set handle pos
-	$ControlHandle.position.y = speed_percent
+	$ControlHandle.position.y = 111 - (111 * speed_float)
 	
 func _clamp_speed(input_f: float) -> float:
 	if input_f > 1: return 1
@@ -34,7 +41,12 @@ func _on_gui_input(ev: InputEvent):
 
 func _on_train_entered(veh3d: Train3D):
 	self.curr_train = veh3d
+	self.curr_speed.target_changed.connect(Callable(self, "_on_target_speed_changed"))
 	
 func _on_train_exited():
+	self.curr_speed.target_changed.disconnect(Callable(self, "_on_target_speed_changed"))
 	self.curr_train = null
+	
+func _on_target_speed_changed(target_speed_perc: float):
+	self._set_handle_position(target_speed_perc)
 #endregion
--- game/gamescripts/ui/vehicle_control/speed_info_panel.gd
+++ game/gamescripts/ui/vehicle_control/speed_info_panel.gd
@@ -1,6 +1,6 @@
 class_name SpeedInfoPanel extends CenterContainer
 
-@export var curr_vehicle: Train3D
+@export var curr_train: Train3D
 
 @export var brake: bool = false
 @export var reverse: bool = false
@@ -9,7 +9,7 @@ class_name SpeedInfoPanel extends CenterContainer
 func _ready() -> void:
 	# connect to vehicle
 	SignalBus.train_entered.connect(Callable(self, "_on_train_entered"))
-	SignalBus.vehicle_exited.connect(Callable(self, "_on_vehicle_exited"))
+	SignalBus.train_exited.connect(Callable(self, "_on_train_exited"))
 	# connect Brake & Reverse buttons
 	%BrakeButton.toggled.connect(Callable(self, "_on_brake_click"))
 	%ReverseButton.toggled.connect(Callable(self, "_on_reverse_click"))
@@ -17,7 +17,8 @@ func _ready() -> void:
 
 func _on_brake_change(new_brake: bool):
 	Loggie.info("Brake mode: %s" % new_brake)
-	
+	if new_brake:
+		self.curr_train.motor.speed.target = 0.0
 func _on_reverse_change(new_reverse: bool):
 	Loggie.info("Reverse mode: %s" % new_reverse)
 	
@@ -30,9 +31,9 @@ func _on_reverse_click(_state: bool):
 	self.reverse = !self.reverse
 	self._on_reverse_change(self.reverse)
 	
-func _on_train_entered(veh3d: Train3D):
-	self.curr_vehicle = veh3d
+func _on_train_entered(train3d: Train3D):
+	self.curr_train = train3d
 	
-func _on_vehicle_exited():
-	self.curr_vehicle = null
+func _on_train_exited():
+	self.curr_train = null
 #endregion
