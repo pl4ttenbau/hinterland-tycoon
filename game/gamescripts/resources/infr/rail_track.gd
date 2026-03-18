6d93115683580131ca6f474070a9ce6c99b21bba
Cornelius Linder <cornelius.linder@onesty-direct.de>
2026-03-18 19:21:56 +0100

    train track generation: refactored iterating through tracks & nodes

--- game/gamescripts/resources/infr/rail_track.gd
+++ game/gamescripts/resources/infr/rail_track.gd
@@ -43,7 +43,8 @@ func build_curve() -> void:
 	# generate AABB & smooth
 	self.abs_aabb = InfrUtils.get_aabb(self.vertices)
 	InfrUtils.smooth_curve3d(self.curve)
-	
+
+#region Distance Check
 func is_near(pos: Vector3) -> bool:
 	if self.get_start_pos().distance_to(pos) <= NEAR_DISTANCE_MAX:
 		return true
@@ -52,6 +53,12 @@ func is_near(pos: Vector3) -> bool:
 	elif self.get_end_pos().distance_to(pos) <= NEAR_DISTANCE_MAX:
 		return true
 	return false
+	
+func get_center_pos() -> Vector3:
+	@warning_ignore("integer_division")
+	var middle_index: int = roundi(self.vertices.size() /2)
+	return self.vertices[middle_index]
+#endregion
 
 #region Get Nodes
 func get_rail_node(_i: int) -> RailNodeData:
@@ -69,16 +76,11 @@ func get_start_pos() -> Vector3:
 func get_end_pos() -> Vector3:
 	var last_i: int = self.nodes.size() -1
 	return self.nodes[last_i].position
-	
-func get_center_pos() -> Vector3:
-	@warning_ignore("integer_division")
-	var middle_index: int = roundi(self.vertices.size() /2)
-	return self.vertices[middle_index]
-	
+
 func has_node_index(_index: int) -> bool:
 	var last_i: int = self.nodes.size() -1
 	return _index >= 0 && _index <= last_i
-	
+
 func get_node_forks() -> Array[RailNodeForkData]:
 	var node_forks: Array[RailNodeForkData] = []
 	if self.nodes[0].fork:
@@ -86,4 +88,24 @@ func get_node_forks() -> Array[RailNodeForkData]:
 	if self.get_end_node().fork:
 		node_forks.append(self.get_end_node().fork)
 	return node_forks
+#endregion
+
+#region Directional Node Getters
+## iterate through all track nodes forwards or backwards & add them into an array
+func get_nodes_directionally(dir: Enums.PathDirection) -> Array[RailNodeData]:
+	var node_list: Array[RailNodeData] = []
+	if dir == Enums.PathDirection.TRACK_NODES_DECREASE:
+		for i in self.nodes.size(): # iterate & add backwards
+			node_list.append(self.nodes[-i-1])
+	else:
+		for node: RailNodeData in self.nodes: # iterate & add forwards
+			node_list.append(node)
+	return node_list
+
+## dont get confused here - the last node backwards is 0, the last node forwards is length - 1
+func get_last_node_directionally(dir: Enums.PathDirection) -> RailNodeData:
+	if dir == Enums.PathDirection.TRACK_NODES_DECREASE:
+		return self.nodes[0]
+	else:
+		return self.nodes[self.nodes.size() -1]
 #endregion
