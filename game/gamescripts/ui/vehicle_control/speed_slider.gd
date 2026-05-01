fcd7f478fb790c50236657a13b0d83a72f6c4dca
Cornelius Linder <cornelius.linder@onesty-direct.de>
2026-05-01 17:15:30 +0200

    fix speed control slider not doing anything

---
+++ game/gamescripts/ui/vehicle_control/speed_slider.gd
@@ -0,0 +1,26 @@
+class_name SpeedSlider extends CenterContainer
+
+@export var current_train: Train3D
+
+func _enter_tree() -> void:
+	SignalBus.player_entered_train.connect(Callable(self, "_on_player_entered_train"))
+	SignalBus.player_exited_train.connect(Callable(self, "_on_player_exited_train"))
+
+func _ready() -> void:
+	$VSlider.value_changed.connect(Callable(self, "_on_speed_slider_change"))
+	
+#region Callbacks
+func _on_speed_slider_change(scaled_speed: float):
+	if self.current_train:
+		var current_speed: VehicleSpeed = self.current_train.motor.speed
+		current_speed.target = scaled_speed
+		Loggie.info("Set target speed to %.1f" % scaled_speed)
+	else:
+		Loggie.warn("Cannot change vehicle speed: no player train registered")
+
+func _on_player_entered_train(train: Train3D):
+	self.current_train = train
+
+func _on_player_exited_train():
+	self.current_train = null
+#endregion
