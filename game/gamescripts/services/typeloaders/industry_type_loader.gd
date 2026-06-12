5b5fa6d6528ff0d8d64c5b7164293990f1146cd8
Cornelius Linder <cornelius.linder@onesty-direct.de>
2026-06-12 16:01:02 +0200

    redone small factory model

--- game/gamescripts/services/typeloaders/industry_type_loader.gd
+++ game/gamescripts/services/typeloaders/industry_type_loader.gd
@@ -18,23 +18,23 @@ func make_types() -> Array[IndustryType]:
 		# producers: liquid
 		IndustryType.new("OIL_WELL", "Oil Well", "oil_derrick", 15, [], ["9xoil"]),
 		# transformers: food
-		IndustryType.new("SUGAR_MILL", "Sugar Mill", "generic_small", 9, ["6xbeets"], ["3xsugar"]),
-		IndustryType.new("BREWERY", "Brewery", "generic_small", 9, ["6xgrains", "2xsugar"], ["3xalcohol"]),
+		IndustryType.new("SUGAR_MILL", "Sugar Mill", "factory_small", 9, ["6xbeets"], ["3xsugar"]),
+		IndustryType.new("BREWERY", "Brewery", "factory_small", 9, ["6xgrains", "2xsugar"], ["3xalcohol"]),
 		IndustryType.new("BAKERY", "Bakery", "restaurant", 5, ["5xgrains", "1xsugar"], ["2xbread"]),
 		IndustryType.new("BUTCHER", "Butcher", "warehouse_1", 9, ["3xfish"], ["3xmeat"]),
 		# transformers: aggregates
-		IndustryType.new("SMELTER", "Smelter", "generic_small", 22, ["3xore", "3xcoal"], ["3xingots"]),
+		IndustryType.new("SMELTER", "Smelter", "factory_small", 22, ["3xore", "3xcoal"], ["3xingots"]),
 		IndustryType.new("SAWMILL", "Sawmill", "warehouse_1", 12, ["9xlumber"], ["3xboards"]),
-		IndustryType.new("BRICKWORKS", "Brickworks", "generic_small", 12, ["5xgravel"], ["2xbricks"]),
+		IndustryType.new("BRICKWORKS", "Brickworks", "factory_small", 12, ["5xgravel"], ["2xbricks"]),
 		IndustryType.new("PAPER_MILL", "Paper Mill", "warehouse_1", 12, ["6xlumber", "2xchemicals"], ["2xpaper"]),
 		IndustryType.new("FERTILIZER_FACTORY", "Fertilizer Factory", "oil_derrick", 12, ["2xchemicals", "5xbeets"], ["2xfertilizer"]),
 		# transformers: smithing
-		IndustryType.new("SMITHY", "Smithy", "generic_small", 12, ["2xingots", "1xcoal", "2xlumber"], ["2xtools", "1xrail_tracks"]),
-		IndustryType.new("WORKSHOP", "Workshop", "generic_small", 12, ["3xingots", "2xcoal"], ["1xweapons", "1xmachines"]),
+		IndustryType.new("SMITHY", "Smithy", "factory_small", 12, ["2xingots", "1xcoal", "2xlumber"], ["2xtools", "1xrail_tracks"]),
+		IndustryType.new("WORKSHOP", "Workshop", "factory_small", 12, ["3xingots", "2xcoal"], ["1xweapons", "1xmachines"]),
 		# transformers: liquid
 		IndustryType.new("OIL_REFINERY", "Refinery", "oil_derrick", 12, ["6xoil"], ["4xchemicals"]),
 		# consumers
-		IndustryType.new("COAL_PLANT", "Coal Plant", "generic_small", 25, ["4xcoal"], []),
+		IndustryType.new("COAL_PLANT", "Coal Plant", "factory_small", 25, ["4xcoal"], []),
 		IndustryType.new("SMALL_STORE", "Small Store", "wooden_storehouse", 10, ["2xbread", "2xmeat", "1xalcohol"], []),
 		IndustryType.new("BIG_STORE", "Big Store", "warehouse_1", 20, ["4xcoal"], []),
 		IndustryType.new("RESTAURANT", "Restaurant", "restaurant", 12, ["2xalcohol", "1xbread", "2xfish"], []),
