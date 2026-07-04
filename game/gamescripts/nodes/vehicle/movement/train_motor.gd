aff5806dde5e33572b479dc28707fd2ffa63b930
Cornelius Linder <cornelius.linder@onesty-direct.de>
2026-07-04 10:28:06 +0200

    new (self-made) wagon: MPSB 600/750mm open wagon

--- game/gamescripts/nodes/vehicle/movement/train_motor.gd
+++ game/gamescripts/nodes/vehicle/movement/train_motor.gd
@@ -1,9 +1,13 @@
 @icon("uid://1bbvrckmpv38")
 class_name TrainMotor extends Node
 
-#region Properties
-@export var MAX_ABS_SPEED: float = .12
+const MAX_ABS_SPEED: float = .12
 
+signal started()
+signal stopped()
+signal reversing()
+
+#region Properties
 @export_storage var speed: VehicleSpeed
 
 @export var current_speed_percentage: float = 0.0:
@@ -12,30 +16,33 @@ class_name TrainMotor extends Node
 
 @export var is_started: bool = false
 
+@export var is_reversed: bool = false:
+	get(): return is_reversed
+	set(value):
+		is_reversed = value
+		self.reversing.emit()
+
 @export var running_time: int:
 	get():
 		if self.is_started && self.running_msec >= 0:
 			return Time.get_ticks_msec() - self.running_msec
 		return -1
-		
+
 @export var direction: Enums.PathDirection
 
 ## engine ticks since this motor was started
 @export var running_msec: int = -1
-
-signal started()
-signal stopped()
 #endregion
 
+#region Initialization
 static func of(_dir: Enums.PathDirection) -> TrainMotor:
 	var _inst = TrainMotor.new()
 	_inst.direction = _dir
 	_inst.speed = VehicleSpeed.new()
 	return _inst
-	
-#func _physics_process(_delta: float) -> void:
-#	Time.get_ticks_msec()
+#endregion
 
+#region Start & Stop
 func start() -> bool:
 	self.is_started = true
 	self.started.emit()
@@ -43,22 +50,22 @@ func start() -> bool:
 	# count ticks since start
 	self.running_msec = Time.get_ticks_msec()
 	return true
-	
+
 func stop() -> void:
 	self.is_started = false
 	self.current_speed_percentage = 0.0
 	self.stopped.emit()
 	# also stop tick counting
 	self.running_msec = -1
-	
+#endregion
+
+#region Callbacks
 func on_motor_tick():
 	if self.speed:
 		self.speed.adjust_to_target_speed()
+#endregion
 
 #region Getters
 func get_current_speed() -> float:
 	return current_speed_percentage * MAX_ABS_SPEED
-	
-func get_speed_vector() -> Vector3:
-	return Vector3(0, 0, -self.get_current_speed())
 #endregion
